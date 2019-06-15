698e74f491ab042bd4c4c79c4120752f0a65a7d8
Lawrence Amer <lawrence.amer@mail.com>
2019-06-15 06:41:28 -0700

    Update config.php

--- web application/application/config/config.php
+++ web application/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '/0xsp/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
