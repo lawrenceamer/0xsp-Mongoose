835aa5ecc4e8b46849f0e785a3fe9557fff88689
Lawrence Amer <lawrence.amer@mail.com>
2019-06-15 05:57:20 -0700

    Update loader.js

--- web application/storage/js/loader.js
+++ web application/storage/js/loader.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     for(var i = 1; i < 9;i++){
         let a = $('.cat'+i);
-        $.get('http://testtesttest234.com/0xsp/dashboard/getResult/'+key+'/'+i,function(data){
+        $.get(host+key+'/'+i,function(data){
             a.text("Total Results: "+data);
         });
     }
