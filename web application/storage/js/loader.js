926876728decfea96e582fb80e0723842be59192
Abboud Dokarli <21991379+abbddkr@users.noreply.github.com>
2019-08-09 23:40:59 +0300

    Update loader.js

--- web application/storage/js/loader.js
+++ web application/storage/js/loader.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    for(var i = 1; i < 9;i++){
+    for(var i = 1; i < categories;i++){
         let a = $('.cat'+i);
         $.get(host+key+'/'+i,function(data){
             a.text("Total Results: "+data);
