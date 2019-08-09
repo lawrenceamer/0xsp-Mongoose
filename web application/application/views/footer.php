73f272cb2a6a77c7690a8de4d9af8f438d7a7ac9
Abboud Dokarli <21991379+abbddkr@users.noreply.github.com>
2019-08-09 23:24:41 +0300

    Update footer.php

--- web application/application/views/footer.php
+++ web application/application/views/footer.php
@@ -21,33 +21,57 @@
   <?php if($this->uri->segment(1) == 'dashboard' && $this->uri->segment(2) == null): ?>
   <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
   <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript">
-$(document).ready(function() {
-        $('#datatable').DataTable( {    
-            "ajax": {
-                "url": "<?=base_url('dashboard/allhosts/')?>",
-                "type": "GET"
-            },            
-            "processing": true,
-            "serverSide": false,
-            "searching": false,
-            "order": [[ 1, "desc" ]],
-            "pageLength": 10,
-                dom: 'Bfrtip',
-            "columns": [    
-                { "data": "random_val"},
-                { "data": "host" },
-                { "data": "sys" },
-                { "data": 'random_val', "width": "10%",
-                render: function ( data, type, report ) {
-                return '<div class="text-center" ><a class="btn btn-primary btn-md" href="<?=base_url('dashboard/scanresult/');?>' + data + '"><i class="fa fa-search"></i></a></div>';
-              } },
-            ],
-        } );
+   <script type="text/javascript">
+    $(document).ready(function() {
+            $('#datatable').DataTable( {    
+                "ajax": {
+                    "url": "<?=base_url('dashboard/getwindowshosts/')?>",
+                    "type": "GET"
+                },            
+                "processing": true,
+                "serverSide": false,
+                "searching": false,
+                "order": [[ 1, "desc" ]],
+                "pageLength": 10,
+                    dom: 'Bfrtip',
+                "columns": [    
+                    { "data": "random_val"},
+                    { "data": "host" },
+                    { "data": "sys" },
+                    { "data": 'random_val', "width": "10%",
+                    render: function ( data, type, report ) {
+                    return '<div class="text-center" ><a class="btn btn-primary btn-md" href="<?=base_url('dashboard/scanresult/');?>' + data + '"><i class="fa fa-search"></i></a></div>';
+                  } },
+                ],
+            } );
 
-});
+    });
+    $(document).ready(function() {
+            $('#datatable2').DataTable( {    
+                "ajax": {
+                    "url": "<?=base_url('dashboard/getlinuxhosts/')?>",
+                    "type": "GET"
+                },            
+                "processing": true,
+                "serverSide": false,
+                "searching": false,
+                "order": [[ 1, "desc" ]],
+                "pageLength": 10,
+                    dom: 'Bfrtip',
+                "columns": [    
+                    { "data": "random_val"},
+                    { "data": "host" },
+                    { "data": "sys" },
+                    { "data": 'random_val', "width": "10%",
+                    render: function ( data, type, report ) {
+                    return '<div class="text-center" ><a class="btn btn-primary btn-md" href="<?=base_url('dashboard/scanresult/');?>' + data + '"><i class="fa fa-search"></i></a></div>';
+                  } },
+                ],
+            } );
+
+    });    
    </script>
-  <?php endif ; ?>
+<?php endif ; ?>
 </body>
 
 </html>
