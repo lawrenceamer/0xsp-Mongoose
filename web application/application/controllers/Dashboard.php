fa2c3d94f030d977ff52d9b27e1bfc2b64509006
Abboud Dokarli <21991379+abbddkr@users.noreply.github.com>
2019-08-09 23:17:28 +0300

    Update Dashboard.php

--- web application/application/controllers/Dashboard.php
+++ web application/application/controllers/Dashboard.php
@@ -28,36 +28,55 @@ class Dashboard extends CI_Controller{
         $data['title'] = "Dashboard";
         $data['resultsnum'] = $this->api_model->getAllResults();
         $data['hostsnum'] = $this->api_model->getAllHosts();
-        $data['categories'] = $this->api_model->getCategories();
-        $data['content'] = "dashboard";
+            $dd = $this->api_model->getHostOsName($host=null);
+            if(strpos($dd,'windows') == true || strpos($dd,'Windows') == true){
+                $data['categories'] = $this->api_model->getWindowsCategories();
+            }else{
+                $data['categories'] = $this->api_model->getLinuxCategories();
+            }        $data['content'] = "dashboard";
         $this->load->view('layout',$data);
     }
-    public function allhosts()
+
+    public function getlinuxhosts()
     {
-        $this->load->library('Datatables');
+        
         $this->datatables->select('random_val,sys,created_at,host,GROUP_CONCAT(id) AS host_id');
-        //$this->datatables->select('host,GROUP_CONCAT(id) AS host_id');
         $this->datatables->from('outputs');
         $this->datatables->where('output_owner',$this->session->userdata('user_id'));
+        $this->db->not_like('sys','Windows');    
+        $this->db->or_not_like('sys','windows');
         $this->datatables->group_by('random_val');
         $this->db->order_by('created_at','DESC');
         echo $this->datatables->generate();    
     }
 
-//     public function scanreshost($host=null,$category=null)
-//     {
-//         $singleresult = $this->api_model->getSingleHostResult($category);
-//         echo json_encode($singleresult);
-//     }
-
+    public function getwindowshosts()
+    {
+        $this->datatables->select('random_val,sys,created_at,host,GROUP_CONCAT(id) AS host_id');
+        //$this->datatables->select('host,GROUP_CONCAT(id) AS host_id');
+        $this->datatables->from('outputs');
+        $this->datatables->like('sys','windows');
+        $this->datatables->or_like('sys','Windows');
+        $this->datatables->where('output_owner',$this->session->userdata('user_id'));
+        $this->datatables->group_by('random_val');
+        $this->db->order_by('created_at','DESC');
+        echo $this->datatables->generate();    
+    }
+    
+ 
     public function scanresult($host = null,$category = null)
     {
         if($host == null){
             redirect('dashboard');
         }
         else if($host != null && $category != null){
+            $dd = $this->api_model->getHostOsName($host);
+            if(strpos($dd,'windows') !== false || strpos($dd,'Windows') !== false){
+                $data['categories'] = $this->api_model->getWindowsCategories();
+            }else{
+                $data['categories'] = $this->api_model->getLinuxCategories();
+            }            
             $data['title'] = "Scan Results";
-            $data['categories'] = $this->api_model->getCategories();
             $data['results'] = $this->api_model->getHostResult($host);
             $data['singleresult'] = $this->api_model->getSingleHostResult($category);
             $data['categoryname'] = $this->api_model->getCategoryName($category);
@@ -66,12 +85,17 @@ class Dashboard extends CI_Controller{
         }
         else{
             $data['title'] = "Scan Results";
-            $data['categories'] = $this->api_model->getCategories();
-            $data['results'] = $this->api_model->getHostResult($host);
+            $dd = $this->api_model->getHostOsName($host);
+            if(strpos($dd,'windows') !== false || strpos($dd,'Windows') !== false){
+                $data['categories'] = $this->api_model->getWindowsCategories();
+            }else{
+                $data['categories'] = $this->api_model->getLinuxCategories();
+            }
             $data['content'] = "scanresults";
             $this->load->view('layout',$data);
         }
     }
+
     function getResult($key=null,$id=null)
     {
         $data = $this->api_model->getCatResult($key,$id);
