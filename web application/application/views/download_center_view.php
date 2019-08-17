602b0080459c92f8458f7bacc3c033ded50bba7c
Abboud Dokarli <21991379+abbddkr@users.noreply.github.com>
2019-08-17 18:18:06 +0000

    Add files via upload

---
+++ web application/application/views/download_center_view.php
@@ -0,0 +1,21 @@
+<div class="container-fluid">
+                           
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800">Download Center</h1>
+        <div class="row">
+        <div class="col-xl-12 col-md-12 mb-4">
+            <table class=" " id="d_center">
+              <thead>
+                <tr class="" >
+                  <th>File Name <i class="fa fa-download"></i></th>
+                  <th>File Type <i class="fa fa-file"></i></th>
+                  <th> <i class="fa fa-cog"></i></th>
+                </tr>
+              </thead>
+              <tbody>
+
+              </tbody>
+            </table>
+        </div>          
+        </div>  
+</div>
