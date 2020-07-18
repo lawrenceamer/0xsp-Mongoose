3e094b0f5100adff5acc274ec494930176e75dc9
Lawrence Amer <contact@lawrenceamer.me>
2020-07-18 23:44:53 +0800

    upgrade into newer version

--- web application/application/views/layout.php
+++
@@ -1,4 +0,0 @@
-<?php $this->load->view('header'); ?>
-<?php $this->load->view('sidebar'); ?>
-<?php $this->load->view($content); ?>
-<?php $this->load->view('footer'); ?>
--- web application/storage/js/loader.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    for(var i = 1; i < categories;i++){
-        let a = $('.cat'+i);
-        $.get(host+key+'/'+i,function(data){
-            a.text("Total Results: "+data);
-        });
-    }
-});
